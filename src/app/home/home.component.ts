321c538fe3ce2d201618cbc52bb508525de6b26d
Mohammad Fahad Alim <105592424+faisalalim1@users.noreply.github.com>
2023-02-24 00:28:14 +0530

    home-page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+import { product } from '../data-type';
+import { ProductService } from '../services/product.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  constructor(private product:ProductService ) {}
+
+  popularProducts:undefined | product[]
+
+  ngOnInit(): void{
+    this.product.popularProducts().subscribe((data)=>{
+      console.log(data);
+      this.popularProducts = data;
+      
+    })
+  }
+
+
 
 }
--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, RouteReuseStrategy } from '@angular/router';
 import { product } from '../data-type';
 import { ProductService } from '../services/product.service';
 
@@ -10,16 +11,20 @@ import { ProductService } from '../services/product.service';
 export class SellerAddProductComponent {
 
   addProductMessage: string | undefined;
-  constructor(private product: ProductService ){}
+  routes: any;
+  constructor(private product: ProductService , private routess: Router ){}
 
   submit(data: product) {
     console.log(data);
     this.product.addProduct(data).subscribe((result)=>{
       console.log(result);
+     
       if(result){
         this.addProductMessage = "Product added successfully"
       }
       setTimeout(()=>(this.addProductMessage = undefined), 3000);
+      this.routess.navigate(['/seller-home'])
+      
       
       
     })
